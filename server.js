163abd580a7213123f0995a6db047b230af57c7e
AbdullahKadad <qdadabdullah@gmail.com>
2024-02-12 12:24:13 +0000

    add/update new routes (/UPDATE, /DELETE, /getMovies)

--- server.js
+++ server.js
@@ -1,26 +1,37 @@
 "use strict";
+// NPM things //
 const express = require("express");
 const app = express();
-const data = require("./MovieData/data.json");
 const axios = require("axios");
 require("dotenv").config();
 const cors = require("cors");
-const { Client } = require("pg");
-const url = `postgres://student:00000111@localhost:5432/movies`;
-const client = new Client(url);
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+// DataBase things //
+const { Client } = require("pg");
+const url = `postgres://${process.env.DBNAME || "student"}:${
+  process.env.DMPASSWORD || "00000111"
+}@localhost:5432/movies`;
+const client = new Client(url);
+
+// PORT //
 const port = process.env.PORT || 3000;
+
+// Json Data //
+const data = require("./MovieData/data.json");
+
+// API //
 const apiKey = process.env.API_KEY;
+// API endpoints //
 const trendingMovies =
   "https://api.themoviedb.org/3/trending/all/week?language=en-US&api_key=";
 const searchMovies = "https://api.themoviedb.org/3/search/movie?api_key=";
 const discoverMovies = "https://api.themoviedb.org/3/discover/movie?api_key=";
 const playingMovies = "https://api.themoviedb.org/3/movie/now_playing?api_key=";
 
-// routes
+// routes //
 app.get("/", homeHandler);
 app.get("/favorite", favoriteHandler);
 app.get("/trending", trendingHandler);
@@ -28,18 +39,21 @@ app.get("/search", searchHandler);
 app.get("/discover", discoverHandler);
 app.get("/playingNow", playingNowHandler);
 app.post("/addMovie", addMovieHandler);
-app.get("/getMovies", getMoviesHandler);
+app.get("/getMovies/:id?", getMoviesHandler);
+app.patch("/UPDATE/:id/comments", updataHandler);
+app.delete("/DELETE/:id", deleteHandler);
 
-// Handlers
+// Handlers //
+//Home
 function homeHandler(req, res) {
   let move = new Move(data.title, data.poster_path, data.overview);
   res.json(move);
 }
-
+//favorite
 function favoriteHandler(req, res) {
   res.send("Welcome to Favorite Page");
 }
-
+//trending
 function trendingHandler(req, res) {
   axios
     .get(`${trendingMovies}${apiKey}`)
@@ -51,7 +65,7 @@ function trendingHandler(req, res) {
       res.status(500).send("Internal Server Error");
     });
 }
-
+//search
 function searchHandler(req, res) {
   const title = req.query.title;
   axios
@@ -64,7 +78,7 @@ function searchHandler(req, res) {
       res.status(500).send("Internal Server Error");
     });
 }
-
+//discover
 function discoverHandler(req, res) {
   const year = req.query.year;
   axios
@@ -77,7 +91,7 @@ function discoverHandler(req, res) {
       res.status(500).send("Internal Server Error");
     });
 }
-
+//playingNow
 function playingNowHandler(req, res) {
   axios
     .get(`${playingMovies}${apiKey}`)
@@ -89,12 +103,12 @@ function playingNowHandler(req, res) {
       res.status(500).send("Internal Server Error");
     });
 }
-
+//addMovie
 function addMovieHandler(req, res) {
-  const { id, title, release_date, poster_path, overview, comments } = req.body;
-  const query = `INSERT INTO Data (id, title, release_date, poster_path, overview, comments)
-  VALUES ($1, $2, $3, $4, $5, $6) RETURNING *;`;
-  const values = [id, title, release_date, poster_path, overview, comments];
+  const { title, release_date, poster_path, overview, comments } = req.body;
+  const query = `INSERT INTO data (title, release_date, poster_path, overview, comments)
+  VALUES ($1, $2, $3, $4, $5) RETURNING *;`;
+  const values = [title, release_date, poster_path, overview, comments];
   client
     .query(query, values)
     .then((result) => {
@@ -105,14 +119,75 @@ function addMovieHandler(req, res) {
       res.status(500).send("Internal Server Error");
     });
 }
-
+//getMovies
 function getMoviesHandler(req, res) {
-  const query = `SELECT * FROM data`;
+  const { id } = req.params;
+  if (id) {
+    const query = `SELECT * FROM data WHERE id = $1`;
+    client
+      .query(query, [id])
+      .then((result) => {
+        if (result.rows.length !== 0) {
+          res.status(200).json(result.rows);
+        } else {
+          res.status(404).send("Movie not found");
+        }
+      })
+      .catch((e) => {
+        console.error(e.stack);
+        res.status(500).send("Internal Server Error");
+      });
+  } else {
+    const query = `SELECT * FROM data`;
+    client
+      .query(query)
+      .then((result) => {
+        res.status(200).json(result.rows);
+      })
+      .catch((e) => {
+        console.error(e.stack);
+        res.status(500).send("Internal Server Error");
+      });
+  }
+}
+//UPDATE
+function updataHandler(req, res) {
+  const id = req.params.id;
+  const { comments } = req.body;
+  const query = `UPDATE data SET comments = $1 WHERE id = $2;`;
+  const values = [comments, id];
   client
-    .query(query)
+    .query(query, values)
+    .then(() => {
+      res.status(200).send(`Movie updated Successfully`);
+    })
+    .catch((e) => {
+      console.log(e.stack);
+      res.status(500).send("Internal Server Error");
+    });
+}
+//DELETE
+function deleteHandler(req, res) {
+  const id = req.params.id;
+  const selectQuery = `SELECT * FROM data WHERE id = $1;`;
+  const query = `DELETE FROM data WHERE id = $1;`;
+  const values = [id];
+  client
+    .query(selectQuery, values)
     .then((result) => {
-      const data = result.rows;
-      res.json(data);
+      if (result.rows.length !== 0) {
+        client
+          .query(query, values)
+          .then(() => {
+            res.status(204).send(`Movie deleted Successfully`);
+          })
+          .catch((e) => {
+            console.log(e.stack);
+            res.status(500).send("Internal Server Error");
+          });
+      } else {
+        return res.status(404).json({ error: "Movie not found." });
+      }
     })
     .catch((e) => {
       console.log(e.stack);
@@ -120,7 +195,7 @@ function getMoviesHandler(req, res) {
     });
 }
 
-// functions
+// functions //
 function reshape(array) {
   let data = [];
   array.forEach((element) => {
@@ -137,14 +212,14 @@ function reshape(array) {
   return data;
 }
 
-// Move constructor
+// Move constructor //
 function Move(title, poster_path, overview) {
   this.title = title;
   this.poster_path = poster_path;
   this.overview = overview;
 }
 
-// API Move constructor
+// API Move constructor //
 function APIMove(id, title, release_date, poster_path, overview) {
   this.id = id;
   this.title = title;
@@ -153,7 +228,7 @@ function APIMove(id, title, release_date, poster_path, overview) {
   this.overview = overview;
 }
 
-// 500 error
+// 500 error //
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({
@@ -162,7 +237,7 @@ app.use((err, req, res, next) => {
   });
 });
 
-// 404 error
+// 404 error //
 app.use((req, res, next) => {
   res.status(404).json({
     status: 404,
@@ -170,7 +245,7 @@ app.use((req, res, next) => {
   });
 });
 
-// listener
+// listener //
 client
   .connect()
   .then(() => {
